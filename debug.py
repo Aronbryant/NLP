5a5bbc9130d0d71abcc198254cea5b3aa75556c9
Aronbryant <67585649+Aronbryant@users.noreply.github.com>
2022-05-10 13:25:34 +0800

    long-token-ner

---
+++ debug.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_coding:utf-8 _*_
+"""=================================================
+@FileName: debug.py
+@Author : yangyunsen
+@Time : 2022/1/25 9:20 上午
+@Desc : 
+================================================="""
+num1 = 5
+print(num1)
+num2 = 10
+print(num2)
